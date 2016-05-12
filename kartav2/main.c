2c77b5579df47de700eb61a885d1aef517229f25
MarcinHilt <inato1@wp.pl>
2016-05-13 01:24:27 +0200

    2

--- kartav2/main.c
+++ kartav2/main.c
@@ -1,3 +1,14 @@
+#include "stm32f4xx_conf.h"
+#include "stm32f4xx_tim.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_syscfg.h"
+#include "misc.h"
+#include "tm_stm32f4_fatfs.h"
+#include "tm_stm32f4_fonts.h
+#include "stm32f4xx.h"
+#include "stm32f4xx_spi.h"
+#include "defines.h"
 
 int main(void) {
 
